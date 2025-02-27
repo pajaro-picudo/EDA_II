2ae8d93d87c63d1f6f584583adcd6e99cd7d139b
angel <alorenzojerez@gmail.com>
2025-02-27 18:41:15 +0100

    avanzando con el filtrado

--- Sesion2-Binary_Heap/monticuloBinario/monticulo.c
+++ Sesion2-Binary_Heap/monticuloBinario/monticulo.c
@@ -6,6 +6,35 @@ void iniciaMonticulo (Monticulo *m)
 {
   m->tamanno = 0;
 }
+
 int vacioMonticulo(Monticulo m) {
-    return m.tamanno == 0;
+     return (m.tamanno == 0) ?  1 : 0;
+}
+
+int insertar(tipoElemento x, Monticulo *m) {
+
+if (m->tamanno >= MAXIMO) {
+
+    printf ("Error: Monticulo ha alcanzado el tamaño máximo\n");
+    return -1;
+
+}
+m->tamanno++;
+m->elemento[m->tamanno] = x;
+filtradoAscendente(&m, x.clave);
+    
+
+
+}
+
+void filtradoAscendente(Monticulo *m, int i) {
+
+int hueco = m->tamanno;
+
+while (m->elemento[hueco/2].clave > i)
+{
+    m->elemento[hueco] = m->elemento[hueco/2];
+    hueco = hueco/2;
+}
+m->elemento[hueco] = i;
 }
