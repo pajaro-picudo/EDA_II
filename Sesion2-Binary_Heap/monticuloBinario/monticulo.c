7d2fd9890834ef3e4a8d5fe18d685519497f5885
pajaro-picudo <alorenzojerez@gmail.com>
2025-04-02 18:19:15 +0200

    cambios

--- Sesion2-Binary_Heap/monticuloBinario/monticulo.c
+++ Sesion2-Binary_Heap/monticuloBinario/monticulo.c
@@ -5,6 +5,7 @@
 void iniciaMonticulo (Monticulo *m)
 {
   m->tamanno = 0;
+  m->elemento[0].clave = -1;
 }
 
 int vacioMonticulo(Monticulo m) {
@@ -21,7 +22,8 @@ if (m->tamanno >= MAXIMO) {
 }
 m->tamanno++;
 m->elemento[m->tamanno] = x;
-filtradoAscendente(&m, x.clave);
+filtradoAscendente(&m, m->tamanno);
+return 0;
     
 
 
@@ -29,12 +31,45 @@ filtradoAscendente(&m, x.clave);
 
 void filtradoAscendente(Monticulo *m, int i) {
 
-int hueco = m->tamanno;
+  tipoElemento e = m->elemento[i];
 
-while (m->elemento[hueco/2].clave > i)
-{
-    m->elemento[hueco] = m->elemento[hueco/2];
-    hueco = hueco/2;
+  while(e.clave < m->elemento[i/2].clave){
+   m->elemento[i] = m->elemento[i/2];
+   i/=2;
+ }
+
+ m->elemento[i] = e;
 }
-m->elemento[hueco] = i;
+
+int eliminarMinimo(Monticulo *m, tipoElemento *minimo){
+	if(m->tamanno <= 0) return -1;
+
+	*minimo = m->elemento[1];
+	m->elemento[1] = m->elemento[m->tamanno];
+	
+	m->tamanno--;
+	filtradoDescendente(m,1);
+
+	return 0;
 }
+
+void filtradoDescendente(Monticulo *m, int i){
+  int hijo, finFiltrado;
+  tipoElemento hueco = m->elemento[i];
+  finFiltrado = 0;
+
+  while(2*i <= m->tamanno && !finFiltrado){
+    hijo = 2*i;
+    if(hijo+1 <= m->tamanno && m->elemento[hijo+1].clave < m->elemento[hijo].clave)
+      hijo++;
+
+    if(hueco.clave > m->elemento[hijo].clave){
+      m->elemento[i] = m->elemento[hijo];
+      i = hijo;
+    }else
+      finFiltrado = 1;
+  }
+
+  m->elemento[i] = hueco;
+}
+
